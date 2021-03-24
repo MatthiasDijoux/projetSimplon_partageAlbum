c984411badaafff883f0d839c697f758d0dc6d22
Morgan O'CONNOR <morgan.oconnor@humapro.fr>
2021-03-24 08:40:57 +0400

    Mise en place page détails photo

---
+++ albums_sharing_app/lib/views/photo.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class PhotoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Photo'),
+      // ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 300,
+                width: 500,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/test.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: null,
+              ),
+              Positioned(
+                top: 25,
+                right: 0,
+                child: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.fullscreen),
+                    onPressed: () {}
+                ),
+              ),
+              Positioned(
+                top: 25,
+                left: 0,
+                child: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {}
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Title(
+                      child: Text('Titre photo'),
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(4),
+                        child: IconButton(
+                            highlightColor: Colors.lightBlue,
+                            icon: Icon(Icons.share),
+                            onPressed: () {}
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(4),
+                        child: IconButton(
+                            icon: Icon(Icons.menu),
+                            onPressed: () {}
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Text("Commentaires :"),
+                    ],
+                  ),
+                ),
+
+                Row(
+                  children: [
+                    Text("Commentaire")
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
