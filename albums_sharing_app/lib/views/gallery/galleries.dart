3ea67612c86e8a4571452b720e2fd4f8f4501dc9
matthiasEntropik <matthias@entropik.digital>
2021-03-24 14:03:02 +0400

    mise en place requete galleries

--- albums_sharing_app/lib/views/gallery/galleries.dart
+++ albums_sharing_app/lib/views/gallery/galleries.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
 import './gallery.dart';
+import '../../models/albums_models.dart';
 
 class GalleriesScreen extends StatelessWidget {
   final List galleries = const [];
@@ -25,8 +26,7 @@ class BuildCard extends StatefulWidget {
 }
 
 class BuildCardState extends State<BuildCard> {
-  String serverResponse = 'Server response';
-
+  List serverResponse;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -40,46 +40,47 @@ class BuildCardState extends State<BuildCard> {
         // );
       },
       child: Card(
-        elevation: 5,
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    serverResponse,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          elevation: 5,
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      'serverResponse',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.share_rounded),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.more_vert),
-                ),
-              ],
-            ),
-            Image.network(
-                'https://images5.alphacoders.com/344/thumb-1920-344761.jpg'),
-          ],
-        ),
-      ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.share_rounded),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.more_vert),
+                  ),
+                ],
+              ),
+              Image.network(
+                  'https://images5.alphacoders.com/344/thumb-1920-344761.jpg'),
+            ],
+          )),
     );
   }
 
-   _makeGetRequest() async {
+  _makeGetRequest() async {
     Response response = await get(_localhost());
     setState(() {
-      serverResponse = response.body;
+      serverResponse = [response.body];
+      serverResponse.map((data) => {print(data)});
     });
   }
 
   Uri _localhost() {
-      return Uri.http('10.0.2.2:3000','/api/albums');
+    return Uri.http('10.0.2.2:3000', '/api/albums');
   }
 }
