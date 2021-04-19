ed5b2632b82d8889dabc42da91744b75e983da66
Morgan O'CONNOR <morgan.oconnor@humapro.fr>
2021-04-19 10:52:47 +0400

    Mise en place upload d'une image

--- albums_sharing_backend/routes/api/index.js
+++ albums_sharing_backend/routes/api/index.js
@@ -5,6 +5,27 @@ const authController = require("../../controllers/authControler");
 const albumController = require("../../controllers/albumController.js");
 const {checkTokenMiddleware} = require("../../middlewares/authMiddlewares")
 const {checkDuplicateEmail} = require('../../middlewares/verifSignup')
+const multer = require('multer');
+const path = require('path');
+const helpers = require("./index");
+
+const handleError = (err, res) => {
+    res
+        .status(500)
+        .contentType("text/plain")
+        .end("Oops! Something went wrong!");
+};
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'src/images');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
 
 /* Formulaire de connexion */
 router.post('/login', authController.login)
@@ -32,12 +53,50 @@ router.post('/secret-route', checkTokenMiddleware, (req, res) => {
 })
 
 /*** Fin Route protégé ***/
-/*  Test POST */
+
+/*** Test Upload de photo ***/
+router.post('/upload', (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('file');
+
+    upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`your image has been uploaded!`);
+    });
+});
+
+const imageFilter = function(req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+exports.imageFilter = imageFilter;
+
+/***  Test POST ***/
 router.post('/test', (req, res) => {
     return res.status(404).json({ message: 'Test POST' })
 })
 
-/*  Test GET */
+/***  Test GET ***/
 router.get('/test', (req, res) => {
     return res.status(404).json({ message: 'Test GET' })
 })
