8a2cdafb0947af26211c501c9f93c7578c41f4ca
matthiasEntropik <matthias@entropik.digital>
2021-04-19 10:33:02 +0400

    mise en place récupèration donnée et affichage

--- albums_sharing_app/lib/views/gallery/galleries.dart
+++ albums_sharing_app/lib/views/gallery/galleries.dart
@@ -28,7 +28,7 @@ class BuildCard extends StatefulWidget {
 }
 
 class BuildCardState extends State<BuildCard> {
-  List _serverResponse;
+  Future<dynamic> _serverResponse;
   void initState() {
     super.initState();
     _serverResponse = _makeGetRequest();
@@ -37,60 +37,74 @@ class BuildCardState extends State<BuildCard> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      child: FutureBuilder(
+          future: _makeGetRequest(),
+          builder: (BuildContext context, AsyncSnapshot res) {
+            if (res.data == null) {
+              return Container(
+                child: Text(''),
+              );
+            }
+            return Container(
+                child: Column(children: prepareCardWidgets(res.data)));
+          }),
       onTap: () {
-        _makeGetRequest().forEach((data) {
-          print(data);
+        _makeGetRequest().then((response) {
+          print(response);
+          _serverResponse = response;
         });
-        // Navigator.push(
-        //   context,
-        //   MaterialPageRoute(builder: (context) {
-        //     return GalleryScreen();
-        //   }),
-        // );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return GalleryScreen();
+          }),
+        );
       },
-      child: Text('salut'),
     );
   }
 
-  _makeGetRequest() async {
-    Response response = await get(_localhost());
-    setState(() {
-      return jsonDecode(response.body);
+  List<Widget> prepareCardWidgets(List<dynamic> datas) {
+    //here you can do any processing you need as long as you return a list of ```Widget```.
+    List<Widget> widgets = [];
+    datas.forEach((item) {
+      widgets.add(Card(
+          elevation: 5,
+          clipBehavior: Clip.antiAlias,
+          child: Column(children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    item['name'],
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.share_rounded),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_vert),
+                ),
+              ],
+            ),
+            Image.network(
+                'https://images5.alphacoders.com/344/thumb-1920-344761.jpg'),
+          ])));
     });
+
+    return widgets;
+  }
+
+  _makeGetRequest() async {
+    final response = await get(_localhost());
+    return jsonDecode(response.body);
   }
 
   Uri _localhost() {
     return Uri.http('10.0.2.2:3000', '/api/albums');
   }
 }
-
-//  Card(
-//           elevation: 5,
-//           clipBehavior: Clip.antiAlias,
-//           child: Column(
-//             children: [
-//               Row(
-//                 children: [
-//                   Padding(
-//                     padding: EdgeInsets.all(16.0),
-//                     child: Text(
-//                       'serverResponse',
-//                       style:
-//                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-//                     ),
-//                   ),
-//                   Spacer(),
-//                   IconButton(
-//                     onPressed: () {},
-//                     icon: Icon(Icons.share_rounded),
-//                   ),
-//                   IconButton(
-//                     onPressed: () {},
-//                     icon: Icon(Icons.more_vert),
-//                   ),
-//                 ],
-//               ),
-//               Image.network(
-//                   'https://images5.alphacoders.com/344/thumb-1920-344761.jpg'),
-//             ],
-//           )),
