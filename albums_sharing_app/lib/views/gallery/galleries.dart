cdd4d8ce88cecc043511c1b045fc95e23afe189d
matthiasEntropik <matthias@entropik.digital>
2021-04-19 08:15:12 +0400

    ajout de slider range

--- albums_sharing_app/lib/views/gallery/galleries.dart
+++ albums_sharing_app/lib/views/gallery/galleries.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -26,12 +28,19 @@ class BuildCard extends StatefulWidget {
 }
 
 class BuildCardState extends State<BuildCard> {
-  List serverResponse;
+  List _serverResponse;
+  void initState() {
+    super.initState();
+    _serverResponse = _makeGetRequest();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        _makeGetRequest();
+        _makeGetRequest().forEach((data) {
+          print(data);
+        });
         // Navigator.push(
         //   context,
         //   MaterialPageRoute(builder: (context) {
@@ -39,44 +48,14 @@ class BuildCardState extends State<BuildCard> {
         //   }),
         // );
       },
-      child: Card(
-          elevation: 5,
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'serverResponse',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.share_rounded),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.more_vert),
-                  ),
-                ],
-              ),
-              Image.network(
-                  'https://images5.alphacoders.com/344/thumb-1920-344761.jpg'),
-            ],
-          )),
+      child: Text('salut'),
     );
   }
 
   _makeGetRequest() async {
     Response response = await get(_localhost());
     setState(() {
-      serverResponse = [response.body];
-      serverResponse.map((data) => {print(data)});
+      return jsonDecode(response.body);
     });
   }
 
@@ -84,3 +63,34 @@ class BuildCardState extends State<BuildCard> {
     return Uri.http('10.0.2.2:3000', '/api/albums');
   }
 }
+
+//  Card(
+//           elevation: 5,
+//           clipBehavior: Clip.antiAlias,
+//           child: Column(
+//             children: [
+//               Row(
+//                 children: [
+//                   Padding(
+//                     padding: EdgeInsets.all(16.0),
+//                     child: Text(
+//                       'serverResponse',
+//                       style:
+//                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+//                     ),
+//                   ),
+//                   Spacer(),
+//                   IconButton(
+//                     onPressed: () {},
+//                     icon: Icon(Icons.share_rounded),
+//                   ),
+//                   IconButton(
+//                     onPressed: () {},
+//                     icon: Icon(Icons.more_vert),
+//                   ),
+//                 ],
+//               ),
+//               Image.network(
+//                   'https://images5.alphacoders.com/344/thumb-1920-344761.jpg'),
+//             ],
+//           )),
